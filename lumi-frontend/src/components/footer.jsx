5306221300bd21b99254a6dd2cb108d64a470a98
Patience Jelagat <patiencejelagatt@gmail.com>
2026-08-05 18:43:40 +0300

    added copyright segment to footer

--- lumi-frontend/src/components/footer.jsx
+++ lumi-frontend/src/components/footer.jsx
@@ -9,54 +9,57 @@ const icons = import.meta.glob( '../assets/icons/*.{png,jpg,jpeg}', {
 export default function Footer(){
     return(
         <footer>
-           <div className='socials'>
-              <div className='lumiLogo'>
-                <img src={icons['../assets/icons/lumi logo.png']} alt="" />
-              </div>
+            <div className='footerContainer'>
+                <div className='socials'>
+                    <div className='lumiLogo'>
+                        <img src={icons['../assets/icons/lumi logo.png']} alt="" />
+                    </div>
 
-              <p>Make room for beautiful living</p>
+                    <p>Make room for beautiful living</p>
 
-              <div className="socialIcons">
-                <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
-                <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
-                <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
-              </div>
+                    <div className="socialIcons">
+                        <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
+                        <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
+                        <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
+                    </div>
 
-           </div>
+                </div>
 
-           <div className="shop">
-             <h4>SHOP</h4>
-           </div>
+                <div className="shop">
+                    <h4>SHOP</h4>
+                </div>
 
-            <div className="shop">
-             <h4>ACCOUNT</h4>
-           </div>
+                <div className="shop">
+                    <h4>ACCOUNT</h4>
+                </div>
 
-           <div className='contactDetails'>
-             <h4>CONTACT</h4>
+                <div className='contactDetails'>
+                    <h4>CONTACT</h4>
 
-             <p>
-                <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
-                Location
-            </p>
-             <p>
-                <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
-                Phone
-            </p>
-             <p>
-                <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
-                Email
-            </p>
-             <p>
-                <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
-                WhatsApp
-            </p>
-           </div> <hr />
+                    <p>
+                        <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
+                        Location
+                    </p>
+                    <p>
+                        <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
+                        Phone
+                    </p>
+                    <p>
+                        <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
+                        Email
+                    </p>
+                    <p>
+                        <img src={icons['../assets/icons/icon placeholder.png']} alt="" />
+                        WhatsApp
+                    </p>
+                </div> 
+            </div> 
 
-           <div className="copyright"> 
+             <hr />
+            <div className="copyright"> 
                <p>2026 LUMI. All rights reserved</p>
                <p>Website by <a href="https://portfolio-murex-two-75.vercel.app/">Patience Jelagat</a></p>
            </div>
-        </footer>        
+        </footer>       
     );
 }
